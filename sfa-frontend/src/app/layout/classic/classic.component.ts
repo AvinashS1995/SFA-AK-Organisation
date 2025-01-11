92a5347155892eb55a042e079ed0bdf374e32218
Kapil-Ingle <codingkapil@gmail.com>
2025-01-11 18:14:01 +0530

    sidenav and footer done, logout dialog done

--- sfa-frontend/src/app/layout/classic/classic.component.ts
+++ sfa-frontend/src/app/layout/classic/classic.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from '../../shared/shared.module';
+import { HeaderComponent } from '../../components/common-ui/header/header.component';
+import { FooterComponent } from '../../components/common-ui/footer/footer.component';
+import { SidenavComponent } from '../../components/common-ui/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-classic',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule, HeaderComponent, FooterComponent, SidenavComponent],
   templateUrl: './classic.component.html',
   styleUrl: './classic.component.scss'
 })
